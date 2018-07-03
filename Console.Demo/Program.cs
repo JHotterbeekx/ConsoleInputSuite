da4e42fcce218562596de70a9d57ae19492585fe
John Hotterbeekx <john.online@hotterbeekx.nl>
2018-07-03 19:16:54 +0200

    Started menu with basic question

--- Console.Demo/Program.cs
+++ Console.Demo/Program.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ConsoleInputSuite;
 
 namespace Console.Demo {
   class Program {
     static void Main(string[] args) {
+
+      new ConsoleInputBuilder()
+        .AddQuestion("Who are you?")
+        .AddQuestion("How old are you?")
+        .AddQuestion("Where do you live?")
+        .Render();
+
+      System.Console.ReadLine();
     }
   }
 }
---
+++ ConsoleInputSuite/ConsoleInputBuilder.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleInputSuite {
+  public class ConsoleInputBuilder {
+
+    private List<string> _Questions = new List<string>();
+    private List<string> _Answers = new List<string>();
+
+    public ConsoleInputBuilder AddQuestion(string text) {
+      _Questions.Add(text);
+      return this;
+    }
+
+    public void Render() {
+      Console.Clear();
+      _Answers = new List<string>();
+
+      foreach (var question in _Questions) {
+        Console.Write($"{question} ");
+        _Answers.Add(Console.ReadLine());
+      }
+
+      _ShowAnwers();
+    }
+
+    private void _ShowAnwers() {
+      Console.Clear();
+      for (int i = 0; i < _Questions.Count; i++) {
+        Console.WriteLine($"{_Questions[i]} ");
+        Console.WriteLine(_Answers[i]);
+      }
+    }
+
+  }
+}
