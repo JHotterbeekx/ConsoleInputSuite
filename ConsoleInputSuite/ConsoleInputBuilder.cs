e2cac600fa294f0319bd9dd944d9f74e83b6d751
John Hotterbeekx <john.online@hotterbeekx.nl>
2018-07-03 20:22:06 +0200

    Refactored question into own class

--- ConsoleInputSuite/ConsoleInputBuilder.cs
+++ ConsoleInputSuite/ConsoleInputBuilder.cs
@@ -1,35 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using ConsoleInputSuite.Input;
 
 namespace ConsoleInputSuite {
   public class ConsoleInputBuilder {
 
-    private List<string> _Questions = new List<string>();
-    private List<string> _Answers = new List<string>();
+    private readonly List<InputQuestion> _Questions = new List<InputQuestion>();
 
     public ConsoleInputBuilder AddQuestion(string text) {
-      _Questions.Add(text);
+      _Questions.Add(new InputQuestion(text));
       return this;
     }
 
     public void Render() {
       Console.Clear();
-      _Answers = new List<string>();
-
-      foreach (var question in _Questions) {
-        Console.Write($"{question} ");
-        _Answers.Add(Console.ReadLine());
-      }
-
+      _Questions.ForEach(x => x.Ask());
       _ShowAnwers();
     }
 
     private void _ShowAnwers() {
       Console.Clear();
-      for (int i = 0; i < _Questions.Count; i++) {
-        Console.WriteLine($"{_Questions[i]} ");
-        Console.WriteLine(_Answers[i]);
-      }
+      _Questions.ForEach(x => x.ShowAnswer());
     }
 
   }
---
+++ ConsoleInputSuite/Input/InputQuestion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleInputSuite.Input {
+  public class InputQuestion {
+
+    private string _Question;
+    private string _Answer;
+
+    public InputQuestion(string question) {
+      _Question = question;
+    }
+
+    public void Ask() {
+      Console.Write($"{_Question} ");
+      _Answer = Console.ReadLine();
+    }
+
+    public void ShowAnswer() {
+      Console.WriteLine(_Question);
+      Console.WriteLine(_Answer);
+    }
+  }
+}
